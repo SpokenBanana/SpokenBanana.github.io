e72590aa774d6d39f70791cb3b65aad027a1f6dc
David Santana <santanavidda@gmail.com>
2015-11-05 01:36:23 -0500

    add ratemygenie info, fun with highlight background also

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
-    var images = ['images/AR_1.png', 'images/AS_1.png', 'images/image.png', 'images/popcircle.png', 'images/sorterMerge.png'];
-    var title =  ['RegexFractals', 'AsteriodPathFinder', 'ImagePalette', 'PhysicsOnPaper', 'Vision Sorter'];
+    var images = ['images/AR_1.png', 'images/AS_1.png', 'images/image.png', 'images/popcircle.png', 'images/sorterMerge.png',
+    'images/screenrmg.png'];
+    var title =  ['RegexFractals', 'AsteriodPathFinder', 'ImagePalette', 'PhysicsOnPaper', 'Vision Sorter',
+    'RateMyGenie'];
 
     setInterval(function(){
         $('#highlight-image').fadeOut(500, function(){
@@ -16,6 +18,15 @@ $(document).ready(function() {
             title.unshift(title.pop());
         }).fadeIn(500);
     }, 10 * 1000);
+
+    $('.highlights').mousemove(function(event) {
+        var obj = $(this);
+        var offset = obj.offset();
+        var x = event.pageX - offset.left;
+        var y = event.pageY - offset.top;
+        obj.css('background-color', 'rgb(' + Math.floor(255 * (x / obj.width())).toString() + ',255,'+
+                Math.floor(255 * (y / obj.height())) + ')');
+    });
 
 });
 
