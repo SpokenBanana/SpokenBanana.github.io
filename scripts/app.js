a0c74cd73d3e0f232e23669a9ae33a1459f481d9
David Santana <santanavidda@gmail.com>
2016-02-28 22:53:19 -0500

    new design

--- scripts/app.js
+++ scripts/app.js
@@ -4,21 +4,8 @@ $(document).ready(function() {
     var title =  ['RegexFractals', 'AsteriodPathFinder', 'ImagePalette', 'PhysicsOnPaper', 'Vision Sorter',
     'RateMyGenie'];
 
-    setInterval(function(){
-        $('#highlight-image').fadeOut(500, function(){
-            $(this).load(function(){
-                $(this).fadeIn(500);
-            });
-            $(this).attr('src', images[0]);
-            images.unshift(images.pop());
 
-        });
-        $('#project-name').fadeOut(500, function(){
-            $('#project-name').text(title[0]);
-            title.unshift(title.pop());
-        }).fadeIn(500);
-    }, 10 * 1000);
-
+    /*
     $('.highlights').mousemove(function(event) {
         var obj = $(this);
         var offset = obj.offset();
@@ -27,6 +14,8 @@ $(document).ready(function() {
         obj.css('background-color', 'rgb(' + Math.floor(255 * (x / obj.width())).toString() + ',255,'+
                 Math.floor(255 * (y / obj.height())) + ')');
     });
+     */
+
 
     var canvas = document.getElementById("canvas");
     canvas.width = 600;
